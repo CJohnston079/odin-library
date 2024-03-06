7635c0b3a9bcfaeeee681672daa0916eeeb1eca0
Chris <chris79gp@outlook.com>
2024-03-06 14:03:09 +0000

    feat: add validateForm() which returns false if any input in a form is empty

--- src/modules/formValidation.js
+++ src/modules/formValidation.js
@@ -1,5 +1,18 @@
-const validateForm = function () {
-	alert("form is valid!");
+const validateForm = function (form) {
+	for (let i = 0; i < form.length; i++) {
+		const element = form[i];
+
+		if (element.tagName !== "INPUT") {
+			continue;
+		}
+
+		if (element.value.trim() === "") {
+			console.log("please fill in blank inputs");
+			return false;
+		}
+	}
+
+	return true;
 };
 
 export default validateForm;
